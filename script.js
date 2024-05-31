808637d5c7a44a8597b0eebb90550f035eb762be
ayocesarallie37 <2202002@utrivieramaya.edu.mx>
2024-05-31 07:11:29 -0600

    update

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// script.js
-
 document.addEventListener('DOMContentLoaded', function() {
     const menuIcon = document.querySelector('.menu-icon');
     const navList = document.querySelector('.nav-list');
